b36d39116fcea8954ab762929f5a8f1ec39140b1
Nick Cross <ncross@redhat.com>
2022-07-13 14:25:14 +0100

    Update to correct Python3 / CentOS 7 dependencies

--- loader.py
+++ loader.py
@@ -3,35 +3,40 @@ class StepsLoader(object):
     def dependencies(params=None):
         deps = {}
 
+        # Fedora/EPEL8 is python3-docker
+        # CentOS 7 / EPEL7 is python36-docker or python-docker-py
         deps['python-docker'] = {
             'library': 'docker',
-            'package': 'python-docker-py',
-            'fedora': {
-                'package': 'python3-docker',
+            'package': 'python3-docker',
+            'centos7': {
+                'package': 'python36-docker',
             }
         }
 
+        # Fedora - python3-behave
+        # EPEL 7 / EPEL 8 : MISSING
         deps['behave'] = {
             'library': 'behave',
-            'package': 'python2-behave',
-            'fedora': {
-                'package': 'python3-behave',
-            }
+            'package': 'python3-behave',
         }
 
+        # Fedora / EPEL 8 : python3-requests
+        # EPEL 7 : python36-requests
         deps['requests'] = {
             'library': 'requests',
-            'package': 'python-requests',
-            'fedora': {
-                'package': 'python3-requests',
+            'package': 'python3-requests',
+            'centos7': {
+                'package': 'python36-requests',
             }
         }
 
+        # Fedora / EPEL 8 : python3-lxml
+        # EPEL 7 : python36-lxml
         deps['lxml'] = {
             'library': 'lxml',
-            'package': 'python-lxml',
-            'fedora': {
-                'package': 'python3-lxml',
+            'package': 'python2-lxml',
+            'centos7': {
+                'package': 'python36-lxml',
             }
         }
 
